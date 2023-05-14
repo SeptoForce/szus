1e440dfd278bb6a8ac8ec6593e747ed20fe01ad6
SeptoForce <septoforcemc@gmail.com>
2023-05-14 18:03:27 +0200

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,49 +1,49 @@
 require("dotenv").config({
-  path: `.env.${process.env.NODE_ENV}`,
+    path: `.env.${process.env.NODE_ENV}`,
 });
 
 const contentfulConfig = {
-  spaceId: process.env.CONTENTFUL_SPACE_ID,
-  accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+    spaceId: process.env.CONTENTFUL_SPACE_ID,
+    accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
 };
 
 if (process.env.CONTENTFUL_HOST) {
-  contentfulConfig.host = process.env.CONTENTFUL_HOST;
-  contentfulConfig.accessToken = process.env.CONTENTFUL_PREVIEW_ACCESS_TOKEN;
+    contentfulConfig.host = process.env.CONTENTFUL_HOST;
+    contentfulConfig.accessToken = process.env.CONTENTFUL_PREVIEW_ACCESS_TOKEN;
 }
 
 const { spaceId, accessToken } = contentfulConfig;
 
 if (!spaceId || !accessToken) {
-  throw new Error(
-    "Contentful spaceId and the access token need to be provided. Received: " +
-      JSON.stringify(contentfulConfig)
-  );
+    throw new Error(
+        "Contentful spaceId and the access token need to be provided. Received: " +
+            JSON.stringify(contentfulConfig)
+    );
 }
 
 // starter config
 module.exports = {
-  siteMetadata: {
-    title: "Gatsby Starter Landing Page",
-    description:
-      "Create custom landing pages using Gatsby and Contentful with this Gatsby Starter",
-  },
-  plugins: [
-    {
-      resolve: `gatsby-theme-landing-page`,
-      options: contentfulConfig,
+    siteMetadata: {
+        title: "Gatsby Starter Landing Page",
+        description:
+            "Create custom landing pages using Gatsby and Contentful with this Gatsby Starter",
     },
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `Gatsby Starter Landing Page`,
-        short_name: `Gatsby Starter Landing Page`,
-        start_url: `/`,
-        background_color: `#fff`,
-        theme_color: `#000`,
-        display: `browser`,
-        icon: `src/assets/gatsby-monogram.png`,
-      },
-    },
-  ],
+    plugins: [
+        {
+            resolve: `gatsby-theme-landing-page`,
+            options: contentfulConfig,
+        },
+        {
+            resolve: `gatsby-plugin-manifest`,
+            options: {
+                name: `Gatsby Starter Landing Page`,
+                short_name: `Gatsby Starter Landing Page`,
+                start_url: `/`,
+                background_color: `#000`,
+                theme_color: `#000`,
+                display: `browser`,
+                icon: `src/assets/gatsby-monogram.png`,
+            },
+        },
+    ],
 };
